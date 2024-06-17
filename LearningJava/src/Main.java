2bca9681bb955abc2bafa359391a45651c07e0f4
Matt Braymer-Hayes <matt.braymerhayes@nbcuni.com>
2024-06-17 16:33:18 -0400

    Multiple choice question

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -2,23 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean valid = false;
-        int inputtedNum = -1;
+        final String question = "What is my address?";
+        final String choiceOne  = "46 Delphi Drive";
+        final String choiceTwo = "1450 Holgate Blvd";
+        final String choiceThree = "15 Georges Way";
 
+        String correctAnswer = choiceThree;
+
+        System.out.printf(
+        "%s\n\t(1) %s\n\t(2) %s\n\t(3) %s\n",
+            question, choiceOne, choiceTwo, choiceThree
+        );
+
+        String choice = null;
         try (Scanner scanner = new Scanner(System.in)) {
-            while (!valid) {
-                System.out.println("Pick a number between 1 and 10");
-                inputtedNum = scanner.nextInt();
-                if (inputtedNum >= 1 & inputtedNum <= 10) {
-                    valid = true;
-                }
-            }
+            choice = scanner.nextLine();
         }
+        choice = choice.toLowerCase();
 
-        if (inputtedNum < 5) {
-            System.out.println("Enjoy the good luck a friend brings you");
+        if (choice.equals(correctAnswer.toLowerCase())) {
+            System.out.println("That's correct!");
         } else {
-            System.out.println("Your shoe selection will make you very happy today");
+            System.out.printf("That is incorrect, the correct answer is %s\n", correctAnswer);
         }
     }
 }
