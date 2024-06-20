8f4cdf43af62d7690c5fb18ba477ba50de23110f
Matt Braymer-Hayes <matt.braymerhayes@nbcuni.com>
2024-06-20 10:24:16 -0400

    Developer tea time

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -2,32 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double area = computeArea();
-        System.out.printf("Area = %.2f\n", area);
+        announceDeveloperTeaTime();
     }
 
-    private static double computeArea() {
-        System.out.println("Let's calculate the area of a triange");
-
-        double base = 0;
-        double height = 0;
+    private static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for developer team time...");
+        System.out.println("Type in a random work and press Enter to start");
         try (Scanner input = new Scanner(System.in)) {
-            System.out.println("Please input the base of the triange (in inches)");
-            base = input.nextDouble(); // bug #1
-            while (base <= 0) {
-                System.out.println("That's invalid. Please input the base of the triange (in inches)");
-                base = input.nextDouble();
-            }
-
-            System.out.println("Please input the height of the triange (in inches)");
-            height = input.nextDouble();
-            while (height <= 0) {
-                System.out.println("That's invalid. Please input the height of the triange (in inches)");
-                height = input.nextDouble(); // bug #2
-            }
+            input.next();
         }
-
-        return base * height / 2;
+        System.out.println("It's developer tea time!");
     }
 }
 
