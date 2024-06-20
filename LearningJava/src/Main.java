0ea55e06505125b00bbf1b6ced48f9f8e3c55232
Matt Braymer-Hayes <matt.braymerhayes@nbcuni.com>
2024-06-20 10:20:29 -0400

    computeArea bugs

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -2,29 +2,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final String question = "What is my address?";
-        final String choiceOne  = "46 Delphi Drive";
-        final String choiceTwo = "1450 Holgate Blvd";
-        final String choiceThree = "15 Georges Way";
+        double area = computeArea();
+        System.out.printf("Area = %.2f\n", area);
+    }
 
-        String correctAnswer = choiceThree;
+    private static double computeArea() {
+        System.out.println("Let's calculate the area of a triange");
 
-        System.out.printf(
-        "%s\n\t(1) %s\n\t(2) %s\n\t(3) %s\n",
-            question, choiceOne, choiceTwo, choiceThree
-        );
+        double base = 0;
+        double height = 0;
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Please input the base of the triange (in inches)");
+            base = input.nextDouble(); // bug #1
+            while (base <= 0) {
+                System.out.println("That's invalid. Please input the base of the triange (in inches)");
+                base = input.nextDouble();
+            }
 
-        String choice = null;
-        try (Scanner scanner = new Scanner(System.in)) {
-            choice = scanner.nextLine();
+            System.out.println("Please input the height of the triange (in inches)");
+            height = input.nextDouble();
+            while (height <= 0) {
+                System.out.println("That's invalid. Please input the height of the triange (in inches)");
+                height = input.nextDouble(); // bug #2
+            }
         }
-        choice = choice.toLowerCase();
 
-        if (choice.equals(correctAnswer.toLowerCase())) {
-            System.out.println("That's correct!");
-        } else {
-            System.out.printf("That is incorrect, the correct answer is %s\n", correctAnswer);
-        }
+        return base * height / 2;
     }
 }
 
